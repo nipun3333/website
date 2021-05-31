67698de7985cf4eb3e2ebd289914444f4f58df35
Nipun3333 <nipunpatel3333@gmail.com>
2021-05-31 17:49:50 +0530

    final date 31st May

--- website_home/urls.py
+++ website_home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('facilities', views.facilities, name="facilities"),
+    path('products', views.products, name="products"),
+    path('contact_us', views.contact_us, name="contact_us"),
+    path('about_us', views.about_us, name="about_us"),
 ]
--- website_home/views.py
+++ website_home/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def facilities(request):
+    return render(request, "facilities.html")
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+def contact_us(request):
+    return render(request, "contact_us.html")
+
+def products(request):
+    return render(request, "products.html")
