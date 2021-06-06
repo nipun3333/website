c179dea4a7211d21cfca52f7847d65576d4dc6fa
Nipun3333 <nipunpatel3333@gmail.com>
2021-06-06 11:38:37 +0530

    6th june 2021

--- website_home/urls.py
+++ website_home/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('facilities', views.facilities, name="facilities"),
-    path('products', views.products, name="products"),
+    path('facilities_products', views.facilities_products, name="facilities_products"),
     path('contact_us', views.contact_us, name="contact_us"),
     path('about_us', views.about_us, name="about_us"),
+    path('condensers', views.condensers, name="condensers"),
+    path('tyre_recycling_plant', views.tyre_recycling_plant, name="tyre_recycling_plant"),
+    path('heat_exchangers', views.heat_exchangers, name="heat_exchangers"),
+    path('industrial_storage_tanks', views.industrial_storage_tanks, name="industrial_storage_tanks"),
+    path('pressure_vessels', views.pressure_vessels, name="pressure_vessels"),
+    path('chimeny', views.chimeny, name="chimeny"),
+    path('industrial_reactors', views.industrial_reactors, name="industrial_reactors"),
+    path('industrial_fabrication_service', views.industrial_fabrication_service, name="industrial_fabrication_service"),
 ]
--- website_home/views.py
+++ website_home/views.py
@@ -1,18 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
-def facilities(request):
-    return render(request, "facilities.html")
+def facilities_products(request):
+    return render(request, "facilities_products.html")
 
 def about_us(request):
     return render(request, "about_us.html")
 
 def contact_us(request):
+
+    if request.method=="POST":
+        myFName = request.POST['myFName']
+        myLName = request.POST['myLName']
+        myEmail = request.POST['myEmail']
+        contact_number = request.POST['contact_number']
+        description = request.POST['description']
+        description+=f"\n Email of {myFName} {myLName} : {myEmail}"
+        description+=f"\n contact Number : {contact_number}"
+        
+        send_mail(
+            f'{myFName} {myLName} wants to contact you', #subject
+            description, #message
+            myEmail, #from email
+            ['nipunpatel2002@gmail.com'], #To email
+        )
+
+        return render(request, "contact_us.html")
+
     return render(request, "contact_us.html")
 
-def products(request):
-    return render(request, "products.html")
+
+def condensers(request):
+    return render(request, "products/condensers.html")
+
+def tyre_recycling_plant(request):
+    return render(request, "products/tyre_recycling_plant.html")
+
+def heat_exchangers(request):
+    return render(request, "products/heat_exchangers.html")
+
+def industrial_storage_tanks(request):
+    return render(request, "products/industrial_storage_tanks.html")
+
+def pressure_vessels(request):
+    return render(request, "products/pressure_vessels.html")
+
+def chimeny(request):
+    return render(request, "products/chimeny.html")
+
+def industrial_reactors(request):
+    return render(request, "products/industrial_reactors.html")
+
+def industrial_fabrication_service(request):
+    return render(request, "products/industrial_fabrication_service.html")
