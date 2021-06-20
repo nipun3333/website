267f7e95d21a443712ff8052f296218009e5bc09
AryanPatel <asp6304@gmail.com>
2021-06-20 13:33:26 +0530

    20/6/2021

--- website_Harsh/settings.py
+++ website_Harsh/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t(-_eb@oczkap#3yfps-dt!wqk(a*1!jq*b6$z+@ypf%c^^87('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
