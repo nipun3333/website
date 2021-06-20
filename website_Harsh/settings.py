26331725104932fa288b83acc29023907c267d2f
AryanPatel182 <69514979+AryanPatel182@users.noreply.github.com>
2021-06-20 13:39:21 +0530

    Update settings.py

--- website_Harsh/settings.py
+++ website_Harsh/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t(-_eb@oczkap#3yfps-dt!wqk(a*1!jq*b6$z+@ypf%c^^87(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','shree-ram-engineers.herokuapp.com/']
 
 
 # Application definition
